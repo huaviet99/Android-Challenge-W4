82dc45388a1d0fc1c954821fd1e6d10940f00262
Viet Hua <huaviet999@gmail.com>
2021-04-18 16:57:26 +0700

    switch between grid and linear layout

--- app/src/main/java/com/thesis/android_challenge_w4/activity/restaurant_list/RestaurantAdapter.kt
+++ app/src/main/java/com/thesis/android_challenge_w4/activity/restaurant_list/RestaurantAdapter.kt
@@ -1,6 +1,5 @@
 package com.thesis.android_challenge_w4.activity.restaurant_list
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,13 +14,38 @@ import com.thesis.android_challenge_w4.model.Restaurant
 
 class RestaurantAdapter :
     ListAdapter<Restaurant, RestaurantAdapter.ViewHolder>(RestaurantDiffUtilCallback()) {
+    companion object {
+        const val LINEAR_ITEM = 0
+        const val GRID_ITEM = 1
+    }
+    private var isViewSwitched = true
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder.from(parent)
+        val inflater = LayoutInflater.from(parent.context)
+        val view : View?
+        if(viewType == LINEAR_ITEM) {
+             view = inflater.inflate(R.layout.item_linear_restaurant, parent, false)
+        } else  {
+             view = inflater.inflate(R.layout.item_grid_restaurant, parent, false)
+        }
+        return ViewHolder(view!!)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
        val item = getItem(position)
         holder.bind(item)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if(isViewSwitched){
+            LINEAR_ITEM
+        } else {
+            GRID_ITEM
+        }
+    }
+
+    fun toggleItemViewType(): Boolean{
+        isViewSwitched = !isViewSwitched
+        return isViewSwitched
     }
 
      class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -32,7 +56,7 @@ class RestaurantAdapter :
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val inflater = LayoutInflater.from(parent.context)
-                val view = inflater.inflate(R.layout.item_restaurant, parent, false)
+                val view = inflater.inflate(R.layout.item_linear_restaurant, parent, false)
                 return ViewHolder(view)
             }
         }
--- app/src/main/java/com/thesis/android_challenge_w4/activity/restaurant_list/RestaurantListActivity.kt
+++ app/src/main/java/com/thesis/android_challenge_w4/activity/restaurant_list/RestaurantListActivity.kt
@@ -1,17 +1,25 @@
 package com.thesis.android_challenge_w4.activity.restaurant_list
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.thesis.android_challenge_w4.R
 import com.thesis.android_challenge_w4.data.getRestaurantList
 import kotlinx.android.synthetic.main.activity_restaurant_list.*
 
+
 class RestaurantListActivity : AppCompatActivity(){
     private lateinit var restaurantAdapter: RestaurantAdapter
-
+    private lateinit var menu: Menu
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(
@@ -19,16 +27,47 @@ class RestaurantListActivity : AppCompatActivity(){
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         setContentView(R.layout.activity_restaurant_list)
+        val toolbar: Toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
         setupRecyclerView()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_restaurant,menu)
+        this.menu = menu!!
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.item_list_switch -> {
+                Log.d("Test","Clicked")
+                val isSwitched: Boolean = restaurantAdapter.toggleItemViewType()
+                if(isSwitched){
+                    rv_restaurant_list.layoutManager = LinearLayoutManager(this)
+                    menu[0].icon = ContextCompat.getDrawable(this,R.drawable.ic_grid)
+                } else {
+                    rv_restaurant_list.layoutManager = GridLayoutManager(this,2)
+                    menu[0].icon = ContextCompat.getDrawable(this,R.drawable.ic_linear)
+
+                }
+            }
+        }
+        return true
+    }
+
     private fun setupRecyclerView(){
-        val linearLayoutManager = GridLayoutManager(this,2)
+        val linearLayoutManager = LinearLayoutManager(this)
         restaurantAdapter = RestaurantAdapter()
         restaurantAdapter.submitList(getRestaurantList())
         rv_restaurant_list.apply {
             layoutManager = linearLayoutManager
             adapter = restaurantAdapter
         }
+    }
+
+    private fun switchIcon(){
+
     }
 }
